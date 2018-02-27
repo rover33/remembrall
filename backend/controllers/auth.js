c418ee829951db622edd2a26b7d15b3737969e8e
rover33 <remy.j.pearlstone@gmail.com>
2018-02-27 10:24:43 -0700

    heroku

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.SECRET);
 }
 
 exports.signin = function(req, res, next) {
