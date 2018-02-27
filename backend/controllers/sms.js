3a31ecd873e5f12615445d01d854a27ddc4cb505
rover33 <remy.j.pearlstone@gmail.com>
2018-02-26 22:24:03 -0700

    styling

--- backend/controllers/sms.js
+++ backend/controllers/sms.js
@@ -13,7 +13,7 @@ exports.sendingMessage = (req,res) => {
     from: process.env.TWILIO_PHONE_NUMBER,
     to: req.body.to,
     body: req.body.body
-})
+    })
 })
     .then((message) => {
         console.log(message.sid)
