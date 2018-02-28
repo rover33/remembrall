a7ce19bb9e03c07f344f699875d499b3f0849198
rover33 <remy.j.pearlstone@gmail.com>
2018-02-28 15:29:10 -0700

    trying go get heroku working

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,16 @@ app.use(bodyParser.json())
 //db
 let db = require('./models')
 
+
+if(!process.env.DYNO) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+  });
+}
 //server static files in public
 // app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, '/build')))
